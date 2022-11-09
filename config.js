583227e6668642d37006efa237d035f925593fe9
Phan Thanh Phú <37042958+Thanhphu84@users.noreply.github.com>
2022-11-09 23:31:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     introTitle: 'Dỗi à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao dỗi cứ đi nhầm đường
-    Lạc vào tim Long lẻ loi
+    Lạc vào tim Phú lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'haha',
     title: 'Phải chăng dỗi đã yêu long ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng dỗi đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Long <33',
-    btnNo: 'Không, Long à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Long <3',
-    btnReply: 'Gửi cho Long <3',
+    btnYes: 'Vẫn cứ là thích Phú <33',
+    btnNo: 'Không, Phú à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Phú <3',
+    btnReply: 'Gửi cho Phú <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Long biết mà 🥰. Yêu dỗi nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h vô chơi game cùng Long nhé công chúa.',
+    mess: 'Phú biết mà 🥰. Yêu dỗi nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình đi ăn nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://m.me/longlee2000'
+    messLink: 'https://www.facebook.com/PhanPhu844'
 }
